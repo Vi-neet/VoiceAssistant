630f05fcc9f005c1918f28a6535afa51a384e0c5
Vineet Bhasin <bhasinvineet49@gmail.com>
2024-04-08 11:41:00 +0530

    opens app or webapp

--- engine/features.py
+++ engine/features.py
@@ -5,6 +5,13 @@ import os
 from engine.command import speak
 import pywhatkit as kit
 import re
+import webbrowser
+import sqlite3
+
+
+
+con = sqlite3.connect("jarvis.db")
+cursor = con.cursor()
 
 # Playing assistant sound function
 @eel.expose
@@ -17,11 +24,36 @@ def openCommand(query):
     query = query.replace("open", "")
     query.lower()
     
-    if query!="":
-        speak("Opening" +query)
-        os.system('start '+query)
-    else:
-        speak("not found")
+    app_name = query.strip()
+
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            results = cursor.fetchall()
+
+            if len(results) != 0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+
+            elif len(results) == 0: 
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                results = cursor.fetchall()
+                
+                if len(results) != 0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("not found")
+        except:
+            speak("some thing went wrong")
 
 def PlayYoutube(query):
     search_term = extract_yt_term(query)
